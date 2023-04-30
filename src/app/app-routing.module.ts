eb23e16f8084010d3d2047eae2e02122440339b1
Daniel Torres <danitecrey@gmail.com>
2023-04-30 00:41:11 -0600

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LibroComponent } from './pages/libro/libro.component';
+import { ListaComponent } from './pages/lista/lista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"lista", component: ListaComponent},
+  {path:'lista/:id', component:LibroComponent},
+  {path:'', redirectTo:'/lista', pathMatch:'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {HttpClientModule} from '@angular/common/http'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ListaComponent } from './pages/lista/lista.component';
+import { LibroComponent } from './pages/libro/libro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    LibroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
